a46090762a8a1b85a9689856ca2c4f6e4c64a950
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-14 07:31:46 +0800

    Update header.php

--- helpdesk_system/includes/header.php
+++ helpdesk_system/includes/header.php
@@ -25,6 +25,7 @@ if (isset($_SESSION['user_id'])) {
     $user_settings = getAllUserSettings($_SESSION['user_id']);
     
     // Count unread announcement notifications
+    $stmt = $db->prepare("SELECT COUNT(*) FROM notifications WHERE user_id = ? AND is_read = 0 AND link = 'announcements.php'");
 }
 
 // Theme settings
