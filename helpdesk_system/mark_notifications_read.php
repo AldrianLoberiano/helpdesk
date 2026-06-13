d7fa4d4bb0bbd5d54ffd576a960c7ac00bae77c0
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 19:41:13 +0800

    Update mark_notifications_read.php

--- helpdesk_system/mark_notifications_read.php
+++ helpdesk_system/mark_notifications_read.php
@@ -9,4 +9,5 @@ require_once __DIR__ . '/includes/auth.php';
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db = getDB();
     $user_id = getCurrentUserId();
