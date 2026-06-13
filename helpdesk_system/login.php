d01d8429b0b4c59bf9b52042f53a87a0c3dd7a08
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 22:59:17 +0800

    Update login.php

--- helpdesk_system/login.php
+++ helpdesk_system/login.php
@@ -96,6 +96,7 @@ $csrf_token = generateCSRFToken();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - <?php echo SITE_NAME; ?></title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
 </head>
