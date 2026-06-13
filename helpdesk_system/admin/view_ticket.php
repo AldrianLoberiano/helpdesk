1c3debb835caf8bc3db55c2500d0003f02d44ea9
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 22:26:01 +0800

    Update view_ticket.php

--- helpdesk_system/admin/view_ticket.php
+++ helpdesk_system/admin/view_ticket.php
@@ -243,7 +243,6 @@ include __DIR__ . '/../includes/navbar.php';
 ?>
 
 <div class="container-fluid py-4">
-    <?php $flash = getFlashMessage(); if ($flash): ?>
         <div class="alert alert-<?php echo $flash['type']; ?> alert-dismissible fade show" role="alert">
             <?php echo $flash['message']; ?>
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
