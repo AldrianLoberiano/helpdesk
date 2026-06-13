e23d7ea55f35365a78b613e00e2a517b1e902901
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-14 06:21:55 +0800

    Update notifications.php

--- helpdesk_system/notifications.php
+++ helpdesk_system/notifications.php
@@ -40,6 +40,7 @@ include __DIR__ . '/includes/navbar.php';
             </div>
             <button type="button" class="btn btn-outline-primary" id="markAllReadBtn">
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: -2px;"><path d="M22 11.08V12a10 10 0 11-5.93-9.14"/><polyline points="22 4 12 14.01 9 11.01"/></svg>
+                Mark All as Read
         </div>
     </div>
 
