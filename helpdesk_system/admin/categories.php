6f374c1f990a80df8baa348215cf167e44864813
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-14 05:31:16 +0800

    Update categories.php

--- helpdesk_system/admin/categories.php
+++ helpdesk_system/admin/categories.php
@@ -164,6 +164,7 @@ include __DIR__ . '/../includes/navbar.php';
                             <td>
                                 <button class="btn btn-sm btn-outline-primary me-1" data-bs-toggle="modal" 
                                         data-bs-target="#editCategoryModal<?php echo $cat['id']; ?>">
+                                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
                                 </button>
                                 <form method="POST" class="d-inline">
                                     <input type="hidden" name="action" value="toggle_status">
