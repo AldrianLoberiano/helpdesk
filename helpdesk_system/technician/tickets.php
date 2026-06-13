7963461a51bfa2680427274c1827254ca4131dcc
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-14 05:36:58 +0800

    Update tickets.php

--- helpdesk_system/technician/tickets.php
+++ helpdesk_system/technician/tickets.php
@@ -57,6 +57,7 @@ include __DIR__ . '/../includes/navbar.php';
             <div>
                 <h1 class="h3 mb-1 fw-bold">My Assigned Tickets</h1>
                 <p class="text-muted mb-0 small">Tickets assigned to you that need attention</p>
+            </div>
         </div>
     </div>
 
