60c692ea148f74e52d8cbcf3ed9fd0c8281554f1
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-14 05:36:43 +0800

    Update tickets.php

--- helpdesk_system/technician/tickets.php
+++ helpdesk_system/technician/tickets.php
@@ -53,7 +53,6 @@ include __DIR__ . '/../includes/navbar.php';
 
 <div class="container-fluid py-4">
     <div class="d-flex justify-content-between align-items-center mb-4">
-            <p class="text-muted mb-0 small">Tickets assigned to you that need attention</p>
         </div>
     </div>
 
