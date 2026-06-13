7ede3a480d2c3c29803530188d61ba463e4f9c2d
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 22:26:27 +0800

    Update profile.php

--- helpdesk_system/profile.php
+++ helpdesk_system/profile.php
@@ -167,7 +167,6 @@ include __DIR__ . '/includes/navbar.php';
         </div>
     </div>
 
-            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         </div>
     <?php endif; ?>
 
