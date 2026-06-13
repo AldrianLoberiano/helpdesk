06ebe041b96700da9f59d59d557227ed11b86869
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 22:20:31 +0800

    Update departments.php

--- helpdesk_system/admin/departments.php
+++ helpdesk_system/admin/departments.php
@@ -116,6 +116,7 @@ include __DIR__ . '/../includes/navbar.php';
             <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#addDepartmentModal">
                 <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: -2px;"><path d="M12 5v14M5 12h14"/></svg>
                 Add Department
+            </button>
         </div>
         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addDepartmentModal">
 Add Department
