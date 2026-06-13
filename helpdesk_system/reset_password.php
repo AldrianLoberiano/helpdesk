ddf05fd162b4e99bc82736dcd18a896dfde3c802
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 22:59:33 +0800

    Update reset_password.php

--- helpdesk_system/reset_password.php
+++ helpdesk_system/reset_password.php
@@ -69,6 +69,7 @@ $csrf_token = generateCSRFToken();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password - <?php echo SITE_NAME; ?></title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="assets/css/style.css" rel="stylesheet">
 </head>
