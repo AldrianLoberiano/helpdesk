af99c09fbcb2449827c785f9e509b16235c92fd8
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 22:27:59 +0800

    Update view_ticket.php

--- helpdesk_system/technician/view_ticket.php
+++ helpdesk_system/technician/view_ticket.php
@@ -216,7 +216,6 @@ include __DIR__ . '/../includes/navbar.php';
 ?>
 
 <div class="container-fluid py-4">
-            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         </div>
     <?php endif; ?>
 
