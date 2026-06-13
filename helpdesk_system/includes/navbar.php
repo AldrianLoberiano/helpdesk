8e8bb46c592a1f82395923b292bfd682f5029b73
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-14 05:35:47 +0800

    Update navbar.php

--- helpdesk_system/includes/navbar.php
+++ helpdesk_system/includes/navbar.php
@@ -69,7 +69,6 @@ foreach (array_slice($name_parts, 0, 2) as $part) {
             <ul class="navbar-nav navbar-right-gap">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle notif-link" href="#" role="button" data-bs-toggle="dropdown">
-                            <span class="badge bg-danger notif-badge"><?php echo $unread_count; ?></span>
                         <?php endif; ?>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end notification-dropdown" style="width: 300px;">
