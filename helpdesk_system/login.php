671fb99c7b5d912ce326f62c959835a60d555cfa
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-13 22:59:24 +0800

    Update login.php

--- helpdesk_system/login.php
+++ helpdesk_system/login.php
@@ -159,7 +159,6 @@ $csrf_token = generateCSRFToken();
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         document.getElementById('togglePassword').addEventListener('click', function() {
             const password = document.getElementById('password');
