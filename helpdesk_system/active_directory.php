963113cbcd1bf44f5ccabd8051da36e478cad9af
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-15 22:17:04 +0800

    Update active_directory.php

--- helpdesk_system/active_directory.php
+++ helpdesk_system/active_directory.php
@@ -60,3 +60,4 @@ include __DIR__ . '/includes/navbar.php';
                             <div class="h4 mb-0 fw-bold text-success"><?php echo $active_users; ?></div>
                         </div>
                     </div>
+                </div>
