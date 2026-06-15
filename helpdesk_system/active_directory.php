35bd5bf5824a539ef9feebda7bf1191b4d0f59dc
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-15 22:14:38 +0800

    Update active_directory.php

--- helpdesk_system/active_directory.php
+++ helpdesk_system/active_directory.php
@@ -32,3 +32,4 @@ include __DIR__ . '/includes/navbar.php';
         <div class="col-md-4">
             <div class="card shadow-sm border-0">
                 <div class="card-body">
+                    <div class="d-flex align-items-center">
