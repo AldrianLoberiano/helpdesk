c2dd5a6f9d661f258875930836e71bcb26beae68
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-15 21:58:44 +0800

    Update navbar.php

--- helpdesk_system/includes/navbar.php
+++ helpdesk_system/includes/navbar.php
@@ -70,6 +70,7 @@ foreach (array_slice($name_parts, 0, 2) as $part) {
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle <?php echo in_array($current_page, ['active_directory.php', 'server_room.php', 'remote_desktop.php']) ? 'active' : ''; ?>" 
                            href="#" role="button" data-bs-toggle="dropdown">Tools</a>
+                        <ul class="dropdown-menu">
             </ul>
             
             <ul class="navbar-nav navbar-right-gap">
