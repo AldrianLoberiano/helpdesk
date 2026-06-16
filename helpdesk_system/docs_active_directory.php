d647a1b3511717298762da11372498f4f08cf602
Aldrian Loberiano <loberianorian@gmail.com>
2026-06-16 23:08:40 +0800

    Update docs_active_directory.php

--- helpdesk_system/docs_active_directory.php
+++ helpdesk_system/docs_active_directory.php
@@ -10,3 +10,4 @@ include __DIR__ . '/includes/navbar.php';
 <div class="container-fluid py-4">
     <div class="row">
         <div class="col-lg-3">
+            <div class="card shadow-sm border-0 sticky-top" style="top: 80px;">
